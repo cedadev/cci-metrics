ba7e28417eadd33279f383b47f8807d61a48aedf
Matthew Richards <matthew.richards@stfc.ac.uk>
2020-01-09 15:20:32 +0000

    Restrict the number of tests being added to prevent timeouts when retrieving results
    - As of writing, the dyamic creation of unit tests created 106 unit tests. This was too high and caused Telegraf to have timeouts when getting the results. A `restrict` attribute has been which only adds the first X tests to stop this from happening

--- ceda/cci_metrics/flask_prometheus_wrapper.py
+++ ceda/cci_metrics/flask_prometheus_wrapper.py
@@ -5,15 +5,34 @@ from ceda.cci_odp_ops_tests.test_esgf_search import EsgfSearchTestCase
 from ceda.cci_odp_ops_tests.test_ftp import FtpTestCase
 from ceda.cci_odp_ops_tests.test_tds_opendap import TdsOpendapTestCase
 
+import configparser
+import os
+
+import logging
+logger = logging.getLogger('waitress')
+
 ''' Wrapper script to launch Flask app, to display results of various CCI services with unit tests
 '''
 
 
-def get_test_names(TestCase, test_sub_title=''):
+def get_test_names(TestCase, test_sub_title='', restrict=None):
     ''' Detects unit test functions for TestCase objects. Functions must begin with 'test'
         Returns a list of test names to be passed into a TestDataContainer object
+
+        `restrict` attribute is used to add only first X (e.g. 50) tests
     '''
-    return [a for a in TestCase.__dict__ if a.startswith('test')]
+    test_name_list = []
+
+    for a, b in zip (TestCase.__dict__, range(len(TestCase.__dict__))):
+        if restrict is not None:
+            # Break before list appending because b starts at 0, existing test names begin at 1
+            if b >= restrict:
+                break
+
+        if a.startswith('test'):
+            test_name_list.append(a)
+
+    return test_name_list
 
 
 cci_data_containers = []
@@ -54,14 +73,14 @@ TdsWmsTestCase = tds_wms_testcase_factory(CATALOG_URI)
 # This means they'll have separate paths - /metrics/TdsCatalogServiceTestCaseWms
 TdsWmsTestCase.__name__ = TdsWmsTestCase.__name__ + 'Wms'
 
-tds_wms_container = TestDataContainer(TdsWmsTestCase, test_names=get_test_names(TdsWmsTestCase),
+tds_wms_container = TestDataContainer(TdsWmsTestCase, test_names=get_test_names(TdsWmsTestCase, restrict=50),
                                       service_name='tds_wms_test_cases')
 cci_data_containers.append(tds_wms_container)
 
 
 TdsWcsTestCase = tds_wcs_testcase_factory(CATALOG_URI)
 TdsWcsTestCase.__name__ = TdsWcsTestCase.__name__ + 'Wcs'
-tds_wcs_container = TestDataContainer(TdsWcsTestCase, test_names=get_test_names(TdsWcsTestCase),
+tds_wcs_container = TestDataContainer(TdsWcsTestCase, test_names=get_test_names(TdsWcsTestCase, restrict=50),
                                       service_name='tds_wcs_test_cases')
 cci_data_containers.append(tds_wcs_container)
 
